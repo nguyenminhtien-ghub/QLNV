2459c5a1f8051d59c6b01518c62f4e4117729242
nguyenminhtien-ghub <minhtiennguyenfriday@gmail.com>
2025-01-17 10:24:13 +0700

    Add project files.

---
+++ QLNV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QLNV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
