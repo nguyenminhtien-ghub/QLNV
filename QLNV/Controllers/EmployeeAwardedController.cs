0f93266334fd67ce22d1883fba5d85937075d00c
nguyenminhtien-ghub <minhtiennguyenfriday@gmail.com>
2025-01-19 23:42:49 +0700

    Add Award management page

---
+++ QLNV/Controllers/EmployeeAwardedController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using QLNV.Data;
+
+namespace QLNV.Controllers;
+
+[Authorize(Policy = "RequireAdministratorRole")]
+public class EmployeeAwardedController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public EmployeeAwardedController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Index()
+    {
+        var awards = _context.EmployeeAwards.ToList();
+        return View(awards);
+    }
+}
