9c7e39be4c607c067aee1508800d1742435c52ad
nguyenminhtien-ghub <minhtiennguyenfriday@gmail.com>
2025-01-19 23:43:23 +0700

    Add employee management page

---
+++ QLNV/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using QLNV.Data;
+
+namespace QLNV.Controllers;
+
+[Authorize(Policy = "RequireAdministratorRole")]
+public class EmployeeController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public EmployeeController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Index()
+    {
+        var employees = _context.Employees.Where(e => e.IsActive).ToList();
+        return View(employees);
+    }
+}
