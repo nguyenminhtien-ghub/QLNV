3f42745c4ae50ec38cb53e5920c277359b7415c7
nguyenminhtien-ghub <minhtiennguyenfriday@gmail.com>
2025-01-19 23:43:58 +0700

    Add salary management page

--- QLNV/Controllers/SalaryController.cs
+++ QLNV/Controllers/SalaryController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using QLNV.Data;
 
 namespace QLNV.Controllers;
 
+[Authorize(Policy = "RequireAdministratorRole")]
 public class SalaryController : Controller
 {
     private readonly ApplicationDbContext _context;
