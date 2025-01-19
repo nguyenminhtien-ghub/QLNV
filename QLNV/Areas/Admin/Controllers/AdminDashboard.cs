583679e84b31464da17421e26bd543a654ed867e
nguyenminhtien-ghub <minhtiennguyenfriday@gmail.com>
2025-01-19 20:43:54 +0700

    Create admin dashboard

---
+++ QLNV/Areas/Admin/Controllers/AdminDashboard.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QLNV.Areas.Admin.Controllers
+{
+    public class AdminDashboard : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
